ac448f2ba3278bd4e1d61c318b58f7d9279693da
aaron-schuyler <asky28@gmail.com>
2020-10-20 15:25:53 -0400

    tweaked cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CLIENT_URL']
+    origins 'inspiring-kirch-11050f.netlify.app'
 
     resource '*',
       headers: :any,
